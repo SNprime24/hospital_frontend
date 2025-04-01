d8f390a842577fdc00ad0541548d7a9ae0640463
Suprit Naik <supritnaik100@gmail.com>
2025-04-01 08:43:07 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import FrontPage from './pages/FrontPage';
+import ErrorPage from './pages/ErrorPage';
+import MainPage from './pages/MainPage';
+import RootLayout from './pages/RootLayout';
+
+import classes from "./App.module.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FrontPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/app",
+    element: <RootLayout />,
+    children: [
+      { path: "", element: <MainPage /> }
+    ]
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/FrontPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FrontPage() {
+    return (
+        <div>
+            FrontPage
+        </div>
+    );
+}
