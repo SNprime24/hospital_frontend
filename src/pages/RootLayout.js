7268e327a68756623c2a8f3582ac62cf04800914
sauravatgithub-web <22cs01010@iitbbs.ac.in>
2025-04-04 19:46:12 +0530

    Enabled LogOut

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import classes from "./RootLayout.module.css";
-
+import axios from "axios";
+import { server } from "../assets/config";
+import toast from "react-hot-toast";
 import NavLogo from "./../assets/AzureMedNavLogo.png";
+import { useDispatch } from "react-redux";
+import { userNotExists } from "../redux/reducers/auth";
 
 export default function RootLayout() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const handleLogOut = async () => {
+    try {
+      const { data } = await axios.get(`${server}/api/v1/auth/logOut`, { withCredentials: true });
+      dispatch(userNotExists());
+      toast.success(data.message);
+      navigate('/');
+    }
+    catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong")
+    }
+  }
 
   return (
     <div className={classes.rootLayout}>
@@ -21,7 +39,7 @@ export default function RootLayout() {
                 <img src={NavLogo} alt="NavgationLogo"/>
             </div>
         </div>
-        <button className={classes.navBarLogoutButton}>Logout</button>
+        <button className={classes.navBarLogoutButton} onClick={handleLogOut}>Logout</button>
       </nav>
 
       <aside className={`${classes.sidebar} ${sidebarOpen ? classes.open : classes.close}`}>
