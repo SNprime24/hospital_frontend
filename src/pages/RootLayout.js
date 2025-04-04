9df408759d0931a00d2fa8fb9933ef5819e8d0a2
Suprit Naik <supritnaik100@gmail.com>
2025-04-04 19:18:37 +0530

    Added Root Layout

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import classes from "./RootLayout.module.css";
 
-// import classes from "./RootLayout.model.css"
+import NavLogo from "./../assets/AzureMedNavLogo.png";
 
 export default function RootLayout() {
-    return (
-        <div>
-            RootLayout
-            <Outlet/>
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className={classes.rootLayout}>
+      <nav className={classes.navbar}>
+        <div className={classes.navbarFirst}>
+            <button 
+                className={classes.menuButton} 
+                onClick={() => setSidebarOpen(prev => !prev)}
+            >
+                ☰
+            </button>
+            <div className={classes.navTitle}>
+                <img src={NavLogo} alt="NavgationLogo"/>
+            </div>
         </div>
-    );
+        <button className={classes.navBarLogoutButton}>Logout</button>
+      </nav>
+
+      <aside className={`${classes.sidebar} ${sidebarOpen ? classes.open : classes.close}`}>
+        lorem ipsum
+      </aside>
+
+      <main className={classes.mainContent}>        
+        <Outlet />
+      </main>
+    </div>
+  );
 }
