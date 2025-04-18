f8e2180b97158f7d19501a5283775995637968ba
Suprit Naik <supritnaik100@gmail.com>
2025-04-18 15:25:01 +0530

    error page added

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import classes from "./ErrorPage.module.css"; // importing CSS module
 
 export default function ErrorPage() {
-    return (
-        <div>
-            ErrorPage
-        </div>
-    );
+  const navigate = useNavigate();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.box}>
+        <h1 className={classes.code}>404</h1>
+        <p className={classes.title}>Oops! Page not found.</p>
+        <p className={classes.message}>
+          The page you're looking for doesn't exist or has been moved.
+        </p>
+        <button className={classes.button} onClick={() => navigate("/")}>
+          Go Home
+        </button>
+      </div>
+    </div>
+  );
 }
